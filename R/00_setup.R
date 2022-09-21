c7e29d3271ead25838ed63125e078dba3e6fcbfd
xander-p <oleksandr.petrenko@mutation.me>
2022-09-21 22:57:46 +0200

    Library and dataset formatting updates

--- R/00_setup.R
+++ R/00_setup.R
@@ -1,6 +1,6 @@
 ## Package installation
 
 ## paths
-datasets <- "data/"
-outputs <- "outputs/tmp/"
-outputs_fin <- "outputs/"
+datasets <- "/data"
+outputs <- "/outputs/tmp"
+outputs_fin <- "/outputs"
