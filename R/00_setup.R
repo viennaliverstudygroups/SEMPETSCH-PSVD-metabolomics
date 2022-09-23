b173782bd7a5faf4f7bfd4f0842981865a8bcee5
xander-p <oleksandr.petrenko@mutation.me>
2022-09-23 18:49:12 +0200

    Exploratory analysis

--- R/00_setup.R
+++ R/00_setup.R
@@ -1,6 +1,10 @@
-## Package installation
+set.seed(42)
 
 ## paths
 datasets <- "/data"
 outputs <- "/outputs/tmp"
 outputs_fin <- "/outputs"
+
+library("Rtsne")
+library("dplyr")
+library("MetaboAnalystR")
