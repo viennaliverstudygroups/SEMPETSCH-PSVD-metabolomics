ce22b544a001f42cd913021b96c8a669f0b198fd
xander-p <oleksandr.petrenko@mutation.me>
2024-09-11 17:48:49 +0200

    acceptance-preparation

--- R/00_setup.R
+++ R/00_setup.R
@@ -1,8 +1,6 @@
 set.seed(42)
-setwd("/research/lab_reiberger/2022_PSVD_metabolomics/2022_PSVD_metabolomics/")
 library("pacman")
 
 ## paths
 datasets <- "/data"
-outputs <- "/outputs/tmp"
-outputs_fin <- "/outputs"
+outputs <- "/outputs"
